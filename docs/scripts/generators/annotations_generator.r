53a2b30b29e263025eacbf27836266f7e8da24b6
gwct <greggwct@gmail.com>
2021-11-05 14:35:11 -0400

    fixed axis labels

--- docs/scripts/generators/annotations_generator.r
+++ docs/scripts/generators/annotations_generator.r
@@ -8,7 +8,7 @@ cat("Rendering annotations.rmd/html\n")
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc/")
 library(rmarkdown)
 library(here)
-#setwd(here("scripts", "generators"))
+setwd(here("docs", "scripts", "generators"))
 print(getwd())
 output_dir = "../.."
 render("../markdown/annotations.rmd", output_dir = output_dir, params = list(output_dir = output_dir), quiet = TRUE)
