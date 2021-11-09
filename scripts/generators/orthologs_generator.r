ec5c6fe5f721b277e404af19de96a930946c3307
gwct <greggwct@gmail.com>
2021-11-09 17:00:41 -0500

    fixed paths

--- scripts/generators/orthologs_generator.r
+++ scripts/generators/orthologs_generator.r
@@ -8,7 +8,7 @@ cat("Rendering orthologs.rmd/html\n")
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc/")
 library(rmarkdown)
 library(here)
-setwd(here("docs", "scripts", "generators"))
+setwd(here("scripts", "generators"))
 print(getwd())
 output_dir = "../.."
 render("../markdown/orthologs.rmd", output_dir = output_dir, params = list(output_dir = output_dir), quiet = TRUE)
