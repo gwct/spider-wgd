70692080cd5a44ca44b364907b6176c540d04fb9
gwct <greggwct@gmail.com>
2022-03-02 16:14:24 -0500

    added link on main page to hox page

--- docs/scripts/generators/index_generator.py
+++ docs/scripts/generators/index_generator.py
@@ -36,6 +36,7 @@ html_template = """
 				<li><h3><a href="alignments.html">Alignment summary stats</a></h3></li>
 				<li><h3><a href="trees.html">Species trees</a></h3></li>
 				<li><h3><a href="grampa_results.html">GRAMPA results</a></h3></li>
+				<li><h3><a href="hox.html">Hox gene trees</a></h3></li>
 			</ul>
 		</div>
         <div class="col-2-24" id="margin"></div>
