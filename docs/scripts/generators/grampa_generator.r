4ce19fd26091fef873003b45d3f4ae42f2d2e2fe
gwct <greggwct@gmail.com>
2022-10-12 10:26:42 -0400

    updated trees and grampa results for 19spec

--- docs/scripts/generators/grampa_generator.r
+++ docs/scripts/generators/grampa_generator.r
@@ -8,7 +8,10 @@ cat("Rendering grampa.rmd/html\n")
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc/")
 library(rmarkdown)
 library(here)
-setwd(here("docs", "scripts", "generators"))
+here()
+#setwd(here("docs", "scripts", "generators"))
+setwd(here())
 print(getwd())
-output_dir = "../.."
-render("../markdown/grampa_results.rmd", output_dir = output_dir, params = list(output_dir = output_dir), quiet = TRUE)
+#output_dir = "../.."
+output_dir = here("docs")
+render(here("docs", "scripts", "markdown", "grampa.rmd"), output_dir = output_dir, params = list(output_dir = output_dir), quiet = TRUE)
