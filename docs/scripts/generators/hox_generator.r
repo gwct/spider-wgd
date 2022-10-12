bd49317043989c4f3f015bf307ccc494c13429ff
gwct <greggwct@gmail.com>
2022-10-12 11:35:00 -0400

    added 19spec hox page

--- docs/scripts/generators/hox_generator.r
+++ docs/scripts/generators/hox_generator.r
@@ -8,7 +8,10 @@ cat("Rendering hox.rmd/html\n")
 Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc/")
 library(rmarkdown)
 library(here)
-setwd(here("docs", "scripts", "generators"))
+here()
+#setwd(here("docs", "scripts", "generators"))
+setwd(here())
 print(getwd())
-output_dir = "../.."
-render("../markdown/hox.rmd", output_dir = output_dir, params = list(output_dir = output_dir), quiet = TRUE)
+#output_dir = "../.."
+output_dir = here("docs")
+render(here("docs", "scripts", "markdown", "hox.rmd"), output_dir = output_dir, params = list(output_dir = output_dir), quiet = TRUE)
